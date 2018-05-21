737f6acb83a7636e5056a5836b442c23df12461b
gabokuck <gabokuck1990@gmail.com>
2018-05-20 19:53:44 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 //Puerto
 //====================================
 process.env.PORT = process.env.PORT || 3000;
+
+//====================================
+//Entorno
+//====================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//====================================
+//Base de Datos
+//====================================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:123456@ds239009.mlab.com:39009/cafe';
+}
+
+process.env.urlDB = urlDB;
